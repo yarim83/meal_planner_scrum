f48e587ac15a85d62314be5a833f5bd360ad8468
hebel1992 <bartosz.hebrycz@gmail.com>
2019-10-10 22:28:03 +0200

    dodanieObslugaFormularzaLogowania

--- src/main/java/pl/coderslab/web/Login.java
+++ src/main/java/pl/coderslab/web/Login.java
@@ -1,16 +1,28 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.AdminDao;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/login")
 public class Login extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
 
+        if (AdminDao.checkLoginData(email, password)) {
+            HttpSession httpSession = request.getSession();
+            httpSession.setAttribute("username", "username");
+            response.sendRedirect("/");
+        } else {
+            response.sendRedirect("/login?msg=Wprowadzono+bledne+dane");
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
