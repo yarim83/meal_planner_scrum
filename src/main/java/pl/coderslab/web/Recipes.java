80e38badbb32be7abcea9bddedcccd31eb097717
Tomasz Smereczynski <tomek.smereczynski@gmail.com>
2019-10-14 20:37:18 +0200

    .

--- src/main/java/pl/coderslab/web/Recipes.java
+++ src/main/java/pl/coderslab/web/Recipes.java
@@ -1,19 +1,24 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.RecipeDao;
+import pl.coderslab.model.Recipe;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/app/recipe/list")
-public class ListRecipe extends HttpServlet {
+public class Recipes extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Recipe> recipesList = RecipeDao.findAll();
+        req.setAttribute("recipeList", recipesList);
 
-
-
+        getServletContext().getRequestDispatcher("/jsp/recipes.jsp").forward(req,resp);
     }
 }
