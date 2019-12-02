eff9c5d15e704bcd78ce0d61b71a5a324059c925
Tomasz Smereczynski <tomek.smereczynski@gmail.com>
2019-12-02 21:18:36 +0100

    Zmiana nazwy metody.

--- src/main/java/pl/coderslab/web/AppRecipes.java
+++ src/main/java/pl/coderslab/web/AppRecipes.java
@@ -4,7 +4,6 @@ import pl.coderslab.dao.RecipeDao;
 import pl.coderslab.model.Admin;
 import pl.coderslab.model.Recipe;
 
-import javax.print.DocFlavor;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/app/recipe/list")
